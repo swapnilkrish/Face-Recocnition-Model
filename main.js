37ee9df895143efe2d0b91bc7a3c25750ae5e5b5
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-02-04 15:16:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2BlUUYkbw/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(ing, gotResult)
+}
+
+function goResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].lable;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3) * 100 + "%";
+    }
+}
